c9bbab77363d24d3101a281b4f670f5b7681914d
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 16:14:07 +0300

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace src;
+
+class Task1 {
+    function main(int $inputNumber): string
+{
+    if ($inputNumber > 30) {
+        return "More than 30";
+    }
+    if ($inputNumber > 20) {
+        return "More than 20";
+    }
+    if ($inputNumber > 10) {
+        return "More than 10";
+    }
+    if ($inputNumber < 10) {
+        return "Less than 10";
+    }
+}
+}
