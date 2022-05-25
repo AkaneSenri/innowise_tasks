7405438e280fa5e696f8e5a1e8bdd2ecc92d0132
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 22:43:17 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -4,17 +4,17 @@ namespace src;
 
 class Task1
 {
-    public function main(int $inputNumber)
+    public function main(int $inputNumber): string
     {
-        $response = $inputNumber > 30
-        ? 'More than 30'
-        : ($inputNumber > 20
-        ? 'More than 20'
-        : ($inputNumber > 10
-        ? 'More than 10'
-        : 'Less than 10!'));
-        echo $inputNumber.' : '.$response."\n";
+        if ($inputNumber > 30) {
+            return 'More than 30';
+        } elseif ($inputNumber > 20) {
+            return 'More than 20';
+        } elseif ($inputNumber > 10) {
+            return 'More than 10';
+        } elseif ($inputNumber < 10) {
+            return 'Less than 10';
+        }
     }
 }
-
 
