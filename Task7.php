cbb410087fa7cb61a8a49079cb1f9f0091df027f
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 21:42:17 +0300

    Update Task7.php

--- Task7.php
+++ Task7.php
@@ -4,14 +4,15 @@ namespace src;
 
 class Task7
 {
-    function main(array $input, int $position): array
+    public function main(array $input, int $position): array
     {
-        if ($position > 0 && (count($input)-1) >= $position && count($input) > 0) {
+        if ($position > 0 && (count($input) - 1) >= $position && count($input) > 0) {
             unset($input[$position]);
             $newArray = [];
             foreach ($input as &$value) {
                 array_push($newArray, $value);
             }
+
             return $newArray;
         } else {
             throw new \InvalidArgumentException;
