b90cc1302375f31c7ffb1ae66ab4fbd458a24374
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 18:27:32 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -2,20 +2,22 @@
 
 namespace src;
 
-class Task1 {
-    function main(int $inputNumber): string
+class Task1
 {
-    if ($inputNumber > 30) {
-        return "More than 30";
-    }
-    if ($inputNumber > 20) {
-        return "More than 20";
-    }
-    if ($inputNumber > 10) {
-        return "More than 10";
-    }
-    if ($inputNumber < 10) {
-        return "Less than 10";
+    function main(int $inputNumber): string
+    {
+        if ($inputNumber > 30) {
+            return "More than 30";
+        }
+        if ($inputNumber > 20) {
+            return "More than 20";
+        }
+        if ($inputNumber > 10) {
+            return "More than 10";
+        }
+        if ($inputNumber < 10) {
+            return "Less than 10";
+        }
     }
 }
-}
+
