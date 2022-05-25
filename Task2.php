47f7c3c9ab742d53a980abf742be0f4b2d58e99f
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 16:20:45 +0300

    Add files via upload

---
+++ Task2.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace src;
+
+use \Datetime;
+
+class Task2
+
+{
+    function main(string $birthday): int
+    {
+        if (preg_match("/^[0-9]{2}[.]{1}[0-9]{2}[.]{1}[0-9]{4}$/", $birthday)) {
+            $dateNow = new \DateTime('today');
+            $bd = new \DateTime($birthday);
+            $bd->setDate($dateNow->format('Y'), $bd->format('m'), $bd->format('d'));
+            $tmp = $dateNow->diff($bd);
+            if ($tmp->invert) {
+                $bd->modify('+1 year');
+                $tmp = $dateNow->diff($bd);
+            }
+            return $tmp->days;
+        } else {
+            throw new \InvalidArgumentException();
+        }
+    }
+}
+
