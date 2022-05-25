26e369555c4b7e8ee1b6fcd44c71ce31a9f9dc33
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 16:21:18 +0300

    Add files via upload

---
+++ Task3.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src;
+
+class Task3
+{
+    function main(int $input): int
+    {
+        if ($input >= 10 && !is_float($input)) {
+            if ($input > 0) {
+                return ($input - 1) % 9 + 1;
+            } else {
+                return 0;
+            }
+        } else {
+            throw new \InvalidArgumentException();
+        }
+    }
+}
