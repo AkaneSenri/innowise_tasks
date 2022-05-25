161a21fd62966fbcb870773fa29f0ed9ad99255c
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-05-25 21:41:52 +0300

    Update Task5.php

--- Task5.php
+++ Task5.php
@@ -1,25 +1,24 @@
-<?php 
+<?php
 
 namespace src;
 
-
-function main(int $input) {
+function main(int $input)
+{
     $one = 0;
     $two = 1;
-    
+
     for ($i = 1; ; $i++) {
-    $current = $one + $two;
-    $one = $two;
-    $two = $current;
-    
-    $length = strlen($current);
+        $current = $one + $two;
+        $one = $two;
+        $two = $current;
+
+        $length = strlen($current);
         if ($length == 100) {
-            Print($current);
+            print($current);
+
             return $current;
-        break;
+
+            break;
         }
     }
 }
-$result = main($input);
-print_r($result);
-?>
