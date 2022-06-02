f31f4afb8c6ffea92001d29a41e166797a29227f
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-06-02 16:09:25 +0300

    Update Task8.php

--- Task8.php
+++ Task8.php
@@ -6,27 +6,34 @@ class Task8
 {
     public function main(string $json): string
     {
-        $jsonInfo = '{
-            "Title": "The Cuckoos Calling",
-            "Author": "Robert Galbraith",
-            "Detail": {
-            "Publisher": "Little Brown"
-            }
-            }
-            ';
+        if (json_last_error() === JSON_ERROR_NONE) {
+            $newData = json_decode($json, true);
 
-        $newData = json_decode($jsonInfo);
-        $newString = '';
-        foreach ($newData as $key => $chunk) {
-            $temp = $key;
-            $temp .= ': ';
-            $temp .= $chunk;
-            $newString .= $temp;
-            if ($chunk !== end($newData)) {
-                $newString .= "\r\n";
+            $my_array = [];
+            $newString = '';
+            foreach ($newData as $key => $chunk) {
+                if (is_array($chunk)) {
+                    $content = $chunk;
+
+                    foreach ($content as $k => $v) {
+                        $temp = $k;
+                        $temp .= ': ';
+                        $temp .= $v;
+                        $newString .= $temp;
+                        $newString .= "\r\n";
+                    }
+                } else {
+                    $temp = $key;
+                    $temp .= ': ';
+                    $temp .= $chunk;
+                    $newString .= $temp;
+                    $newString .= "\r\n";
+                }
             }
+
+            return $newString;
+        } else {
+            throw new \InvalidArgumentException;
         }
-
-        return $newString;
     }
 }
